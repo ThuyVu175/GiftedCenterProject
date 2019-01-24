6b6cf37bfac8870e85676af49468a431fd453d8b
ThuyVu175 <thuyvtt620@gmail.com>
2019-01-24 15:38:50 +0700

    huhu

---
+++ Project/GiftedCenterProject/GiftedCenterProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GiftedCenterProject.Startup))]
+namespace GiftedCenterProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
