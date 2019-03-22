f5c6827c7dd71dee990f55e83212d29738ceaf35
ThuyVu175 <thuyvtt620@gmail.com>
2019-03-22 16:28:21 +0700

    fix login

--- Project/GiftedCenterProject/GiftedCenterProject/Scripts/Ajax.js
+++ Project/GiftedCenterProject/GiftedCenterProject/Scripts/Ajax.js
@@ -2,11 +2,15 @@
 function Login() {
     var Username = $("#txtUsername").val();
     var Password = $("#txtPassword").val();
+    if (Username == null || Password == null) {
+        alert("Vui lòng nhập đầy đủ thông tin!");
+    }
     $.ajax({
         url: "/Login/Login",
         data:{  Username: Username, Password: Password},
         success: function (result) {
-            if (result == 1) {
+          
+            if (result == 0) {
                // alert("ddd");
                 window.location = "/Home/DashBoard";
             }
