f8082da3c02cd695956d2ecdd92c9abedbb7046d
ThuyVu <thuyvtt620@gmail.com>
2019-04-23 17:49:59 +0700

    add user

---
+++ Source/GiftedCenterProject/GiftedCenterProject/Controllers/LectureController.cs
@@ -0,0 +1,33 @@
+﻿using GiftedCenterProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GiftedCenterProject.Controllers
+{
+    public class LectureController : Controller
+    {
+        Connect cnn = new Connect();
+        // GET: Lecture
+        public ActionResult Index()
+        {
+            var lecturer = from a in cnn.Lecturer
+                           where a.isActive.Equals(1)
+                           select a;
+            if(lecturer!= null)
+            {
+                return View(lecturer.ToList());
+            }
+            return View();
+
+            var countLecturer = (from count in cnn.Lecturer
+                                 where count.isActive.Equals(1)
+                                 select count).Count();
+            
+        }
+
+
+    }
+}
---
+++ Source/GiftedCenterProject/GiftedCenterProject/Scripts/Ajax.js
@@ -0,0 +1,29 @@
+﻿//Login
+function Login() {
+    var Username = $("#txtUsername").val();
+    var Password = $("#txtPassword").val();
+    if (Username =="" || Password == "") {
+        alert("Vui lòng nhập đầy đủ thông tin!");
+    }
+    else {
+        $.ajax({
+            url: "/Login/Login",
+            data: { Username: Username, Password: Password },
+            success: function (result) {
+
+                if (result == 1) {
+                    // alert("ddd");
+                    window.location = "/Home/DashBoard";
+                }
+                else {
+                    alert("Xem lại Tài khoản hoặc mật khẩu!");
+                }
+
+            }
+        });
+
+    }
+  
+}
+
+
